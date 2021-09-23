c66a0c209cba083aaf499dc2e9e8ce9e4d96bf9f
joaovmlima <oi.oaoj@gmail.com>
2021-09-22 22:00:50 -0300

    initial commit

---
+++ README.rst
@@ -0,0 +1,28 @@
+============
+SonicOS API
+============
+
+************
+About
+************
+This library provides functions to communicate with SonicWall's Firewall API, without the need to build the requests by hand.
+
+It is built on firmware 6.5.4, compatibility with 7.0.0 should be fine but its yet to be tested.
+
+| Currently only the HTTP Basic login method is supported.
+| If you use HTTPS, it is secure enough ;)
+
+***********
+Examples
+***********
+| ``fwLogin("https://192.168.1.1", "admin", "password", False)``
+| Logs into the firewall for executing the other functions.
+
+| ``getCFSLists("192.168.1.1", False)``
+| Returns all the Content Filter lists configured in the firewall at 192.168.1.1
+
+***********
+Supported Actions
+***********
+| Currently, this lib is focused on the CFS feature of the firewall, so there's a very limited number of functions to other features.
+| I'm hoping to implement it along the way, feel free to help :)
---
+++ sonicos_api/sonicOS.py
@@ -0,0 +1,130 @@
+import json
+import base64
+import requests
+from collections import OrderedDict
+
+headers = OrderedDict(
+        [('Accept', 'application/json'),
+            ('Content-Type', 'application/json'),
+            ('Accept-Encoding', 'application/json'),
+            ('charset', 'UTF-8')
+         ])
+
+def fwLogin(fwAddress: str, fwUser: str, fwPassword: str, verifySSLCert: bool):
+    # make credentials base64 encoded
+    credentials = fwUser+":"+fwPassword
+    credentials = bytes(credentials, 'utf-8')
+    encode = base64.b64encode(credentials)
+    encode = str(encode, 'utf-8')
+
+    url = f"{fwAddress}/api/sonicos/auth"
+    payload = ""
+    headers = OrderedDict(
+        [('Authorization', f'Basic {encode}'),
+            ('Accept', 'application/json'),
+            ('Content-Type', 'application/json'),
+            ('Accept-Encoding', 'application/json'),
+            ('charset', 'UTF-8')
+         ])
+
+    response = requests.request("POST", url, data=payload, headers=headers, verify=verifySSLCert)
+    response = response.json()
+    response = json.dumps(response).loads(response)
+
+    return response
+
+def fwLogout(fwAddress: str, verifySSLCert: bool):
+    url = f"{fwAddress}/api/sonicos/auth"
+    payload = ""
+
+    response = requests.request("DELETE", url, data=payload, headers=headers, verify=verifySSLCert)
+    response = response.json()
+    response = json.dumps(response).loads(response)
+
+    return response
+
+def configMode(fwAddress: str, verifySSLCert: bool):
+    url = f"{fwAddress}/api/sonicos/config-mode"
+    payload = ""
+    response = requests.request("POST", url, headers=headers, data=payload, verify=verifySSLCert)
+    response = response.json()
+    response = json.dumps(response).loads(response)
+
+    return response
+
+def commitChanges(fwAddress: str, verifySSLCert: bool):
+    url = f"{fwAddress}/api/sonicos/config/pending"
+    payload = ""
+    response = requests.request("POST", url, headers=headers, data=payload, verify=verifySSLCert)
+    response = response.json()
+    response = json.dumps(response).loads(response)
+
+    return response
+
+def getCFSProfiles(fwAddress: str, verifySSLCert: bool):
+    url = f"{fwAddress}/api/sonicos/content-filter/profiles"
+
+    response = requests.request("GET", url, headers=headers, verify=verifySSLCert)
+    response = response.json()
+    response = json.dumps(response).loads(response)
+    
+    return response
+
+def getCFSLists(fwAddress: str, verifySSLCert: bool):
+    url = f"{fwAddress}/api/sonicos/content-filter/uri-list-objects"
+    payload = ""
+    response = requests.request("GET", url, headers=headers, data=payload, verify=verifySSLCert)
+    response = response.json()
+    response = json.dumps(response).loads(response)
+    
+    return response
+
+def getSpecificCFSList(fwAddress: str, cfsListName: str, verifySSLCert: bool):
+    url = f"{fwAddress}/api/sonicos/content-filter/uri-list-objects/name/{cfsListName}"
+    payload = ""
+    response = requests.request("GET", url, headers=headers, data=payload, verify=verifySSLCert)
+    response = response.json()
+    response = json.dumps(response).loads(response)
+    
+    return response
+
+def insertIntoCFSList(fwAddress: str, cfsName: str, uri: str, verifySSLCert: bool):
+    url = f"{fwAddress}/api/sonicos/content-filter/uri-list-objects"
+    payload = {"content_filter": {"uri_list_object": [
+        {
+            "name": cfsName,
+                    "uri": [{"uri": uri}]
+        }
+    ]}}
+
+    response = requests.request("PUT", url, json=payload, headers=headers, verify=verifySSLCert)
+    response = response.json()
+    response = json.dumps(response).loads(response)
+
+    return response
+
+def removeFromCFS(fwAddress: str, cfsName: str, uri: str, verifySSLCert: bool):
+    url = f"{fwAddress}/api/sonicos/content-filter/uri-list-objects"
+
+    payload = {"content_filter": {"uri_list_object": [
+                {
+                    "name": cfsName,
+                    "uri": [{"uri": uri}]
+                }
+            ]}}
+
+    response = requests.request("DELETE", url, json=payload, headers=headers, verify=verifySSLCert)
+    response = response.json()
+    response = json.dumps(response).loads(response)
+
+    return response
+
+def getFwInfo(fwAddress, verifySSLCert: bool):
+    url = f"{fwAddress}/api/sonicos/administration/global"
+
+    payload = ""
+    response = requests.request("GET", url, headers=headers, data=payload, verify=verifySSLCert)
+    response = response.json()
+    response = json.dumps(response).loads(response)
+    
+    return response.json()
