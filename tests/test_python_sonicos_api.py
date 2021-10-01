33042d46af52a2cfef41cd24be95c2565c6a0412
joaovmlima <oi.oaoj@gmail.com>
2021-10-01 17:55:05 -0300

    add tests

--- tests/test_python_sonicos_api.py
+++ tests/test_python_sonicos_api.py
@@ -1,5 +1,30 @@
-from python_sonicos_api import __version__
+from sonicos_api import __version__
+from sonicos_api import sonicOS as snwl
 
+address = 'https://192.168.168.168:6443'
+user = 'admin'
+password = 'password'
 
-def test_version():
-    assert __version__ == '0.1.0'
+def testLogin():
+    response = snwl.fwLogin(address, user, password, False)
+    assert response['status']['success'] == True
+
+def testCommit():
+    response = snwl.commitChanges(address, False)
+    assert response['status']['success'] == True
+
+def testGetCFSProfiles():
+    response = snwl.getCFSProfiles(address, False)
+    assert 'content_filter' in response
+
+def testGetCFSList():
+    response = snwl.getCFSLists(address, False)
+    assert 'content_filter' in response
+
+def testGetSpecificCFSList():
+    response = snwl.getSpecificCFSList(address, 'Allowed Default', False)
+    assert 'content_filter' in response
+
+def testLogout():
+    response = snwl.fwLogout(address, False)
+    assert response['status']['success'] == True
