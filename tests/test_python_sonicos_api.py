77254dbe17a71706f71d825b3d1cc527f3e874c3
joaovmlima <oi.oaoj@gmail.com>
2021-10-04 17:47:59 -0300

    add more tests

--- tests/test_python_sonicos_api.py
+++ tests/test_python_sonicos_api.py
@@ -1,7 +1,7 @@
 from sonicos_api import __version__
 from sonicos_api import sonicOS as snwl
 
-address = 'https://192.168.168.168:6443'
+address = 'https://192.168.168.168:3443'
 user = 'admin'
 password = 'password'
 
@@ -24,6 +24,18 @@ def testGetCFSList():
 def testGetSpecificCFSList():
     response = snwl.getSpecificCFSList(address, 'Allowed Default', False)
     assert 'content_filter' in response
+
+def testInsertIntoCFSList():
+    response = snwl.insertIntoCFSList(address, "Allowed Default", "test.rebex.net", False)
+    assert response['status']['success'] == True
+
+def testRemoveFromCFSList():
+    response = snwl.removeFromCFS(address, "Allowed Default", "test.rebex.net", False)
+    assert response['status']['success'] == True
+
+def testGetFwInfo():
+    response = snwl.getFwInfo(address, False)
+    assert response['status']['success'] == True
 
 def testLogout():
     response = snwl.fwLogout(address, False)
